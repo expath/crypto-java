b173c894e8ab3116a0d038f7d739db82033a76f2
claudius108 <claudius.teodorescu@gmail.com>
2015-03-13 00:49:45 +0100

    Renamed a method.

--- src/main/java/ro/kuberam/libs/java/crypto/keyManagement/GenerateKeyPair.java
+++ src/main/java/ro/kuberam/libs/java/crypto/keyManagement/GenerateKeyPair.java
@@ -3,20 +3,27 @@ package ro.kuberam.libs.java.crypto.keyManagement;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 
-import ro.kuberam.libs.java.crypto.randomSequencesGeneration.GenerateRandomNumber;
+import ro.kuberam.libs.java.crypto.randomSequencesGeneration.RandomNumber;
 
 public class GenerateKeyPair {
-	
-	public static KeyPair generateKeyPair(long seed, String algorithm, String provider) throws Exception {
+
+	public static KeyPair generate(String algorithm) throws Exception {
 		KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("DSA");
-		keyGenerator.initialize(1024, GenerateRandomNumber.generateRandomNumber(seed, "SHA1PRNG", provider));
+		keyGenerator.initialize(1024, RandomNumber.generate("SHA1PRNG", "SUN"));
 
 		return (keyGenerator.generateKeyPair());
 	}
-	
+
+	public static KeyPair generate(long seed, String algorithm, String provider) throws Exception {
+		KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("DSA");
+		keyGenerator.initialize(1024, RandomNumber.generate(seed, "SHA1PRNG", provider));
+
+		return (keyGenerator.generateKeyPair());
+	}
+
 	public static void main(String args[]) throws Exception {
 
-		KeyPair keyPair = generateKeyPair(1008, "SHA1PRNG", "base64");
+		KeyPair keyPair = generate(1008, "SHA1PRNG", "base64");
 
 		System.out.println("Private key:\n" + keyPair.getPrivate());
 
